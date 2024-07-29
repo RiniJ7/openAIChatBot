92d49128a4bb01d7c1b3e3fea37ae442889533a6
rinij <rinimani7@gmail.com>
2024-07-29 00:07:37 -0600

    comments added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import { config } from "dotenv"
 config()
+//if you want to use the modules inside node, you have to set type as module
+//dotenv package loads the environment variables from a .env file to process.env file
 
 console.log(process.env.API_KEY)
