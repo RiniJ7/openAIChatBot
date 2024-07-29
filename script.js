c6638861d29557d7a880b18acadc9a39c9fbb76a
rinij <rinimani7@gmail.com>
2024-07-29 09:00:02 -0600

    async function added , input output added API endpoint configured

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 import { config } from "dotenv"
 config()
+
+
 //if you want to use the modules inside node, you have to set type as module
 //dotenv package loads the environment variables from a .env file to process.env file
+import { OpenAI } from "openai";
+import readline from "readline";
 
-console.log(process.env.API_KEY)
+
+// console.log(process.env.API_KEY)
+
+const openai = new OpenAI({ apiKey: process.env.API_KEY });
+
+async function main() {
+    const userInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+    
+
+    userInterface.question('Please enter your prompt' , async (input) => {
+        try {
+            const completion = await openai.chat.completions.create({
+                messages: [{ role: "user", content: input }],
+                model: "gpt-4o-mini",
+                max_tokens:5,
+              });
+              console.log(completion.choices[0].message.content.trim());
+        } catch (error) {
+            if(error.response) {
+                console.error('Error with OpenAI API:', error.response.data);
+            } else {
+                console.error('Error', error.message);
+            }
+        } finally {
+            userInterface.close();
+        }
+    });
+
+}
+
+
+main();
